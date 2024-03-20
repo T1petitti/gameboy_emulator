11d8afacdce112dc54ee892cebee8748caeba8ca
COMP406/590 <comp406590@bridgewater>
2024-03-20 11:15:01 -0400

    Added part 2 without scroll, possibly working, no qt to test

--- gameboy.cpp
+++ gameboy.cpp
@@ -2,11 +2,17 @@
 #include <fstream>
 //#include <vector>
 #include "Z80.h"
-//include "screen.h"
+#include "gameboy.pro"
+#include "screen.h"
+#include "screen.cpp"
 
 // Global ROM array - Will be loaded from a file
 unsigned char* rom; // Use unsigned char* for binary data
 int romSize = 0;
+unsigned char graphicsRAM[8192];
+int palette[4];
+int tileset, tilemap, scrollx, scrolly;
+
 
 // Memory access functions
 unsigned char memoryRead(int address) {
@@ -20,7 +26,12 @@ void memoryWrite(int address, unsigned char b) {
     // For this project, writing to ROM is not handled
 }
 
-int main() {
+extern QApplication* app;
+extern Screen* screen;
+int main(int argc, char** argv) {
+
+    setup(argc,argv);
+    //part 1 code here
     // Load ROM from file
     std::ifstream romfile("testrom.gb", std::ios::in | std::ios::binary | std::ios::ate);
     if (!romfile.is_open()) {
@@ -50,6 +61,37 @@ int main() {
     // Output the final value in register A (expected: 21)
     std::cout << "Final A: " << (int)z80->A << std::endl;
 
+    //return 0;
+    //part 2 code here
+    // read the first 8192 integers from screendump into graphicsRAM.
+    if (!vidfile.is_open()) {
+        std::cerr << "Unable to open screendump.txt" << std::endl;
+        return 1;
+    }
+    int n;
+    ifstream vidfile("screendump.txt",ios::in);
+    for(int i=0; i<8192; i++){
+        int n;
 
-    return 0;
+        vidfile>>n;
+
+        graphicsRAM[i]=(unsigned char)n;
+    }
+    // Then read the other variables:
+
+    vidfile >> tileset;
+    vidfile >> tilemap;
+    vidfile >> scrollx;
+    vidfile >> scrolly;
+    vidfile >> palette[0];
+    vidfile >> palette[1];
+    vidfile >> palette[2];
+    vidfile >> palette[3];
+
+    // Now that graphicsRAM and other variables are initialized, call renderScreen
+    renderScreen();
+
+    // Enter the Qt application loop
+    return app.exec();
 }
+ 
